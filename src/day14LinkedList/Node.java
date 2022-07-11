689d41cbc0162f64018a15aa3b73d4137cddc33e
Hiteshmeher <meher.hitesh4@gmail.com>
2022-07-11 23:12:59 +0530

    created a simple linked list

---
+++ src/day14LinkedList/Node.java
@@ -0,0 +1,18 @@
+package day14LinkedList;
+/**
+ * 
+ * @author LENOVO
+ *
+ */
+public class Node {
+	
+	int data;
+	Node next;
+/**
+ * 
+ * @param data initialize variable
+ */
+	Node(int data) {
+		this.data = data;
+	}
+}
---
+++ src/day14LinkedList/main.java
@@ -0,0 +1,27 @@
+package day14LinkedList;
+
+/**
+ * 
+ * @author LENOVO
+ *
+ */
+public class main {
+	/**
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		/**
+		 * creating object of linkedlist class
+		 * added values throgh add method
+		 * display values through display method
+		 */
+
+		Linkedlist list = new Linkedlist();
+		list.add(56);
+		list.add(30);
+		list.add(70);
+		list.display();
+	}
+}
